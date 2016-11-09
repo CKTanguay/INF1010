ba0dd8321d6c8d8f652c1c69c4e6eeafe8f67e40
CKTanguay <CKTanguay@gmail.com>
2016-11-09 09:21:36 -0500

    Creation classe monde magique

---
+++ TP5/INF1010_TP5/INF1010_TP5/MondeMagique.h
@@ -0,0 +1,25 @@
+#ifndef MONDE_MAGIQUE_H
+#define MONDE_MAGIQUE_H
+
+#include <iostream>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
+template<typename T, typename S>
+
+class MondeMagique
+{
+public:
+	list<T*> listMaitre_;
+	list<S*> listCompagnon_;
+
+
+private:
+
+
+};
+
+
+#endif
