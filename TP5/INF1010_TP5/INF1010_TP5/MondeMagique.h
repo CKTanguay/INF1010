fff56c56ad1b4a4f25b5545f3f8109c39d1be07f
CKTanguay <CKTanguay@gmail.com>
2016-11-09 10:34:42 -0500

    implementation monde magique

--- TP5/INF1010_TP5/INF1010_TP5/MondeMagique.h
+++ TP5/INF1010_TP5/INF1010_TP5/MondeMagique.h
@@ -12,14 +12,43 @@ template<typename T, typename S>
 class MondeMagique
 {
 public:
-	list<T*> listMaitre_;
-	list<S*> listCompagnon_;
+	MondeMagique();
+	~MondeMagique();
 
+	list<T*> getListMaitre() const;
+
+	void setListMaitre(const T& maitre);
+
+	list<S*> getListCompagnon() const;
+
+	void setListCompagnon(const S& compagnon);
+
+	void ajouter(const T& maitre);
+
+	void ajouter(const S& compagnon);
+
+	void supprimer(const T& maitre);
+
+	void supprimer(const S& compagnon);
+
+	//ne pas oublier de creer un iterateur pour find!!
+
+	void supprimerMaitre()
 
 private:
-
+	list<T*> listMaitre_;
+	list<S*> listCompagnon_;
 
 };
 
 
+template<typename T, typename S>
+MondeMagique<T, S>::MondeMagique()
+{
+
+}
+
+
+
 #endif
+
