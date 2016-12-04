733a0a2f9b5a4e718dd77ac35efbecb6930eaa0a
CKTanguay <CKTanguay@gmail.com>
2016-12-04 15:48:32 -0500

    ajout fichiers zip tp6

--- TP6/Fichiers_TP6/TP6/exceptionattaqueechouee.cpp
+++
@@ -1,3 +0,0 @@
-#include "ExceptionAttaqueEchouee.h"
-
-//ExceptionAttaqueEchouee::ExceptionAttaqueEchouee(const std::string & what_arg = "") : std::runtime_error(what_arg) {}
--- TP6/Fichiers_TP6/TP6/exceptionecheccapture.cpp
+++
@@ -1,4 +0,0 @@
-#include "ExceptionEchecCapture.h"
-
-//ExceptionEchecCapture::ExceptionEchecCapture(const std::string & what_arg = "") : std::runtime_error(what_arg) {}
-
