101f9f986ff087f90fb9f4ab4bf3c23381911e69
maxz0ne <maxime.brousseau@polymtl.ca>
2016-12-03 21:04:08 -0500

    max sa marcher sur laptop apres acoir commenter 2 ligne

--- TP6/Fichiers_TP6/TP6/exceptionecheccapture.cpp
+++ TP6/Fichiers_TP6/TP6/exceptionecheccapture.cpp
@@ -1,4 +1,4 @@
 #include "ExceptionEchecCapture.h"
 
-ExceptionEchecCapture::ExceptionEchecCapture(const std::string & what_arg = "") : std::runtime_error(what_arg) {}
+//ExceptionEchecCapture::ExceptionEchecCapture(const std::string & what_arg = "") : std::runtime_error(what_arg) {}
 
